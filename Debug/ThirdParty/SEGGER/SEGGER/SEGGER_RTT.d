457137b6a3b71d5085d1227f22998a9864ea4c4b
doraditya nandanamudi <doraditya.nandanamudi@nanothingsinc.com>
2019-03-24 17:11:26 -0400

    Adding gitignore

--- Debug/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.d
+++ Debug/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.d
@@ -1,8 +1,8 @@
 ThirdParty/SEGGER/SEGGER/SEGGER_RTT.o: \
  ../ThirdParty/SEGGER/SEGGER/SEGGER_RTT.c \
  ../ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
- C:/Users/doraditya/RTOS_Workspace_2/STM32_HelloWorld/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
+ C:/Users/doraditya/RTOS_Workspace_2/STM32_HelloWorld-v0.0.1/freeRTOS-hw/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
 
 ../ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
 
-C:/Users/doraditya/RTOS_Workspace_2/STM32_HelloWorld/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
+C:/Users/doraditya/RTOS_Workspace_2/STM32_HelloWorld-v0.0.1/freeRTOS-hw/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
